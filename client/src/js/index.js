d8957d7ffdbad6b866fae1bcc55aa5e5c9140ad1
unknown <mcsalazar122@gmail.com>
2022-09-10 23:32:26 -0500

    webpack installed

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,6 +2,9 @@ import { Workbox } from 'workbox-window';
 import Editor from './editor';
 import './database';
 import '../css/style.css';
+import './header';
+import './install';
+import '../css/style.css'
 
 const main = document.querySelector('#main');
 main.innerHTML = '';
